3eb7569d84dda6c0059f98ca884a8b9c7d03b256
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-04-23 18:30:00 +0530

    login page and 404 page completed

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function toggleMenu(){
    document.querySelector(".menus").classList.toggle("show");
 }
 
-// window.onload = function(){
-//    document.getElementById("home-page").scrollIntoView();
-// }
+function login(){
+   confirm("Login successfull")
+}
