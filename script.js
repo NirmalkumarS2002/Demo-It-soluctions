f056e2dc474d8e9ec0a71f3c345af50fc761abcc
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-04-28 23:49:23 +0530

    about page completed

--- script.js
+++ script.js
@@ -94,3 +94,13 @@ subbutton.addEventListener("click", () => {
 
 })
 
+
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+}, 2500);
+
